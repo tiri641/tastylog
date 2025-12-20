6bec089d9fb0a590a1f6bcc1de161edbfc48c6a0
myhearisranch <myhearisranch@gmail.com>
2025-12-20 17:02:04 +0900

    IAMロールにタグを追加

--- 10_infra/iam.tf
+++ 10_infra/iam.tf
@@ -17,6 +17,9 @@ resource "aws_iam_role" "app_iam_role" {
     Name    = "${var.project}-${var.environment}-app-iam-role"
     Project = var.project
     Env     = var.environment
+
+    # タグを追加する
+    Message = "Hello"
   }
 }
 
